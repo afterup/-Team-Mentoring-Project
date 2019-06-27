26a06c1f38f0f8e3a562b6dc253daecebf868f03
ForA <afterup@naver.com>
2019-06-27 16:53:54 +0900

    JoinForm 수정

--- src/com/firstjava/view/JoinForm.java
+++ src/com/firstjava/view/JoinForm.java
@@ -13,10 +13,12 @@ import java.awt.Font;
 
 public class JoinForm extends JFrame {
 
-	public JTextField tf_id, tf_name, tf_ssn1, tf_phone1, tf_phone2, tf_phone3, tf_email;
-	public JPasswordField tf_pass, tf_pass2, tf_ssn2;
+	public JTextField tf_id, tf_name, tf_phone1, tf_phone2, tf_phone3, tf_email;
+	public JPasswordField tf_pass, tf_pass2;
 	public JButton bt_submit, bt_cancel, bt_checkid;
-	JLabel la_ID, la_pass, la_passCheck, la_name, la_ssn, la_phone, la_email, la_validCheck;
+	JLabel la_ID, la_pass, la_passCheck, la_name, la_phone, la_email, la_validCheck;
+	private JLabel lblNewLabel;
+	private JLabel label;
 
 	public JoinForm() {
 
@@ -24,7 +26,6 @@ public class JoinForm extends JFrame {
 
 		tf_id = new JTextField();
 		tf_name = new JTextField();
-		tf_ssn1 = new JTextField();
 		tf_phone1 = new JTextField();
 		tf_phone2 = new JTextField();
 		tf_phone3 = new JTextField();
@@ -32,7 +33,6 @@ public class JoinForm extends JFrame {
 
 		tf_pass = new JPasswordField();
 		tf_pass2 = new JPasswordField();
-		tf_ssn2 = new JPasswordField();
 
 		bt_submit = new JButton("등록");
 		bt_cancel = new JButton("취소");
@@ -44,14 +44,11 @@ public class JoinForm extends JFrame {
 		tf_pass2.setBounds(117, 261, 100, 25);
 		tf_name.setBounds(117, 301, 100, 25);
 
-		tf_ssn1.setBounds(117, 341, 50, 25);
-		tf_ssn2.setBounds(177, 341, 50, 25);
+		tf_phone1.setBounds(117, 338, 40, 25);
+		tf_phone2.setBounds(167, 338, 40, 25);
+		tf_phone3.setBounds(217, 338, 40, 25);
 
-		tf_phone1.setBounds(117, 381, 40, 25);
-		tf_phone2.setBounds(167, 381, 40, 25);
-		tf_phone3.setBounds(217, 381, 40, 25);
-
-		tf_email.setBounds(117, 421, 200, 25);
+		tf_email.setBounds(117, 378, 200, 25);
 
 		bt_submit.setBounds(74, 499, 90, 34);
 		bt_cancel.setBounds(174, 499, 90, 34);
@@ -63,8 +60,6 @@ public class JoinForm extends JFrame {
 		getContentPane().add(tf_pass);
 		getContentPane().add(tf_pass2);
 		getContentPane().add(tf_name);
-		getContentPane().add(tf_ssn1);
-		getContentPane().add(tf_ssn2);
 		getContentPane().add(tf_phone1);
 		getContentPane().add(tf_phone2);
 		getContentPane().add(tf_phone3);
@@ -94,25 +89,33 @@ public class JoinForm extends JFrame {
 		la_name.setBounds(30, 303, 90, 15);
 		getContentPane().add(la_name);
 
-		la_ssn = new JLabel("주민번호 :");
-		la_ssn.setFont(new Font("맑은 고딕", Font.PLAIN, 15));
-		la_ssn.setBounds(30, 343, 90, 15);
-		getContentPane().add(la_ssn);
-
 		la_phone = new JLabel("휴대번호 :");
 		la_phone.setFont(new Font("맑은 고딕", Font.PLAIN, 15));
-		la_phone.setBounds(30, 383, 90, 15);
+		la_phone.setBounds(30, 340, 90, 15);
 		getContentPane().add(la_phone);
 
 		la_email = new JLabel("이메일주소 :");
 		la_email.setFont(new Font("맑은 고딕", Font.PLAIN, 15));
-		la_email.setBounds(30, 423, 90, 15);
+		la_email.setBounds(30, 380, 90, 15);
 		getContentPane().add(la_email);
 
 		la_validCheck = new JLabel("유효성 검사");
 		la_validCheck.setFont(new Font("맑은 고딕", Font.PLAIN, 15));
-		la_validCheck.setBounds(127, 466, 100, 15);
+		la_validCheck.setBounds(128, 449, 100, 15);
 		getContentPane().add(la_validCheck);
+		
+		JLabel la_join = new JLabel("회 원 가 입");
+		la_join.setFont(new Font("맑은 고딕", Font.PLAIN, 20));
+		la_join.setBounds(117, 62, 108, 41);
+		getContentPane().add(la_join);
+		
+		lblNewLabel = new JLabel("-");
+		lblNewLabel.setBounds(157, 342, 8, 18);
+		getContentPane().add(lblNewLabel);
+		
+		label = new JLabel("-");
+		label.setBounds(207, 341, 8, 18);
+		getContentPane().add(label);
 
 		setVisible(false);
 		setBounds(1280, 170, 360, 600);
@@ -127,8 +130,6 @@ public class JoinForm extends JFrame {
 	       tf_pass.setText("");
 	       tf_pass2.setText("");
 	       tf_name.setText("");
-	       tf_ssn1.setText(""); 
-	       tf_ssn2.setText("");
 	       tf_phone1.setText(""); 
 	       tf_phone2.setText(""); 
 	       tf_phone3.setText("");
@@ -164,5 +165,4 @@ public class JoinForm extends JFrame {
 			}
 		});
 	}
-
 }
